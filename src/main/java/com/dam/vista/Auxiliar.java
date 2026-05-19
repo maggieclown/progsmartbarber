a1bc9b78fd2a82028833f389f2e323fcf1899dd7
Maggie <noexistorealmenteosi@gmail.com>
2026-05-20 01:39:53 +0200

    v3.0

---
+++ src/main/java/com/dam/vista/Auxiliar.java
@@ -0,0 +1,65 @@
+import java.util.List;
+
+/*public class Auxiliar {// PRECARGA DE DATOS
+public static void preCarga(List<Cliente> clientes, List<Producto> productos, List<Factura> compras, List<Factura> prestamos) {
+    try {
+        Cliente cliente = new Cliente();
+        cliente.setDni("97602871W");
+        cliente.setNombre("Eduardo");
+        cliente.setApellidos("Cano");
+        cliente.setTelefono("612345678");
+        cliente.setEmail("edu@gmail.com");
+        cliente.altaCliente(clientes);
+    } catch (Exception e) {
+        System.out.println("Error al registrar el cliente 1: " + e.getMessage());
+    }
+
+    try {
+        Cliente cliente2 = new Cliente();
+        cliente2.setDni("85109842K");
+        cliente2.setNombre("Sergio");
+        cliente2.setApellidos("Valera");
+        cliente2.setTelefono("623456789");
+        cliente2.setEmail("sergio@gmail.com");
+        cliente2.altaCliente(clientes);
+    } catch (Exception e) {
+        System.out.println("Error al registrar el cliente 2: " + e.getMessage());
+        @@ -90,33 +90,34 @@
+                servicio1.setId(001);
+        servicio1.setNombre("Corte");
+        servicio1.setDescripcion("Corte de calidad");
+        servicio1.getPrecio(10)
+        servicio1.getPrecio(10);
+        servicio1.altaServicio(servicios);
+    } catch (Exception e) {
+        System.out.println("Error al registrar el servicio 1: " + e.getMessage());
+    }
+
+    try {
+        Servicio servicio2 = new Servicio();
+        servicio2.setId(002);
+        servicio2.setNombre("Afeitado");
+        servicio2.setDescripcion("Afeitado de gran calidad");
+        servicio2.getPrecio(12)
+        servicio2.getPrecio(12);
+        servicio2.altaServicio(servicios);
+    } catch (Exception e) {
+        System.out.println("Error al registrar el servicio 2: " + e.getMessage());
+    }
+
+    try {
+        Servicio servicio3 = new Servicio();
+        servicio3.setId(003);
+        servicio3.setNombre("Afeitado + corte");
+        servicio3.setDescripcion("Opción para gente que quiere ahorrar un poco mas pero con las misma calidad");
+        servicio3.getPrecio(16)
+        servicio3.getPrecio(16);
+        servicio3.altaServicio(servicios);
+    } catch (Exception e) {
+        System.out.println("Error al registrar el servicio 3: " + e.getMessage());
+    }
+}
+
+}
+*/
+//...Validaciones...
