4bde40ee1e538fa76f8d8d2ab7c45fcf42925f7c
Sergio <noexistorealmenteosi@gmail.com>
2026-05-20 01:42:53 +0200

    v4.0

--- src/main/java/com/dam/modelo/ConexionBD.java
+++ src/main/java/com/dam/modelo/ConexionBD.java
@@ -26,43 +26,52 @@ public class ConexionBD {
     private static void crearTablas() throws Exception {
         String sql;
         try (Statement st = conn.createStatement()) {
-            sql = "CREATE TABLE IF NOT EXISTS Clientes ("
-                    + "dni VARCHAR(10) NOT NULL,"
-                    + "nombre VARCHAR(50) NOT NULL,"
-                    + "CONSTRAINT pk_Profesores PRIMARY KEY (dni) )";
-            st.executeUpdate(sql);
             sql = "CREATE TABLE IF NOT EXISTS Empleados ("
-                    + "id INTEGER NOT NULL,"
-                    + "titulo VARCHAR(50) NOT NULL,"
-                    + "horas DECIMAL(6,2) NOT NULL,"
-                    + "dniProfesor VARCHAR(10) NULL,"
-                    + "fecIni DATE NULL,"
-                    + "fecFin DATE NULL,"
-                    + "modalidad CHAR(1) NOT NULL,"
-                    + "estado VARCHAR(12) NULL,"
-                    + "CONSTRAINT pk_Cursos PRIMARY KEY (id),"
-                    + "CONSTRAINT fk_Cursos_Profesores FOREIGN KEY (dniProfesor)"
-                    + " REFERENCES Profesores (dni) ON DELETE SET NULL )";
+                    + "dni VARCHAR(9) NOT NULL,"
+                    + "nombre VARCHAR(20) NOT NULL,"
+                    + "apellidos VARCHAR(20) NOT NULL,"
+                    + "telefono VARCHAR(9),"
+                    + "CONSTRAINT pk_Empleados PRIMARY KEY (dni) )";
+            st.executeUpdate(sql);
+            sql = "CREATE TABLE IF NOT EXISTS Clientes ("
+                    + "dni VARCHAR(9) NOT NULL,"
+                    + "nombre VARCHAR(20) NOT NULL,"
+                    + "apellidos VARCHAR(20) NOT NULL,"
+                    + "telefono VARCHAR(9),"
+                    + "email VARCHAR(25) NOT NULL UNIQUE,"
+                    + "CONSTRAINT pk_Clientes PRIMARY KEY (dni) )";
             st.executeUpdate(sql);
             sql = "CREATE TABLE IF NOT EXISTS Facturas ("
-                    + "dni VARCHAR(10) NOT NULL,"
-                    + "nombre VARCHAR(50) NOT NULL,"
-                    + "mayorEdad BOOLEAN NOT NULL,"
-                    + "CONSTRAINT pk_Alumnos PRIMARY KEY (dni) )";
+                    + "id INTEGER,"
+                    + "fecha DATE NOT NULL,"
+                    + "total DECIMAL(6,2) NOT NULL,"
+                    + "dni_cliente VARCHAR(9) NOT NULL,"
+                    + "id_servicio INTEGER NOT NULL,"
+                    + "id_productos INTEGER NOT NULL,"
+                    + "CONSTRAINT fk_dni_cliente FOREIGN KEY (dni_cliente) REFERENCES Clientes(dni) ON DELETE NO ACTION,"
+                    + "CONSTRAINT fk_id_servicio FOREIGN KEY (id_servicio) REFERENCES Servicios(id) ON DELETE NO ACTION,"
+                    + "CONSTRAINT fk_id_producto FOREIGN KEY (id_producto) REFERENCES Productos(dni) ON DELETE NO ACTION,"
+                    + "CONSTRAINT fk_dni_empleado FOREIGN KEY (dni_empleado) REFERENCES Empleados(dni) ON DELETE NO ACTION,"
+                    + "CONSTRAINT pk_Facturas PRIMARY KEY (dni) )";
             st.executeUpdate(sql);
             sql = "CREATE TABLE IF NOT EXISTS Productos ("
-                    + "idCurso INTEGER NOT NULL,"
-                    + "dniAlumno VARCHAR(10) NOT NULL,"
-                    + "CONSTRAINT pk_Matriculas PRIMARY KEY (idCurso,dniAlumno),"
-                    + "CONSTRAINT fk_Matriculas_Cursos FOREIGN KEY (idCurso)"
-                    + " REFERENCES Cursos (id) ON DELETE CASCADE,"
-                    + "CONSTRAINT fk_Matriculas_Alumnos FOREIGN KEY (dniAlumno)"
-                    + " REFERENCES Alumnos (dni) ON DELETE CASCADE )";
+                    + "id INTEGER NOT NULL,"
+                    + "nombre VARCHAR(20) NOT NULL,"
+                    + "precio DECIMAL(6,2) NOT NULL,"
+                    + "stock INTEGER NOT NULL,"
+                    + "dni_empleado VARCHAR(9) NOT NULL,"
+                    + "CONSTRAINT pk_Productos PRIMARY KEY (id) ,"
+                    + "CONSTRAINT chk_Productos CHECK (stock >= 0) ,"
+                    + "CONSTRAINT fk_Productos FOREIGN KEY (dni_empleado) REFERENCES Empleados (dni)  ON DELETE CASCADE)";
             st.executeUpdate(sql);
             sql = "CREATE TABLE IF NOT EXISTS Servicios ("
-                    + "dni VARCHAR(10) NOT NULL,"
-                    + "nombre VARCHAR(50) NOT NULL,"
-                    + "CONSTRAINT pk_Profesores PRIMARY KEY (dni) )";
+                    + "id INTEGER PRIMARY KEY,"
+                    + "nombre VARCHAR(30) NOT NULL,"
+                    + "descripion VARCHAR(30),"
+                    + "precio DECIMAL(6,2) NOT NULL,"
+                    + "tipo VARCHAR(20) NOT NULL, "
+                    + "CONSTRAINT chK_Servicios CHECK(precio >0),"
+                    + "CONSTRAINT pk_Servicios PRIMARY KEY (id) )";
             st.executeUpdate(sql);
         } catch (SQLException e) {
             throw new Exception("Error crearTablas()!!", e);
